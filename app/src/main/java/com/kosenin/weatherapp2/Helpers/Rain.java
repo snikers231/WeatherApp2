6164215847e509370b5f82829d221d3c23e94b35
kosenin <kosenin@yandex.ru>
2017-06-28 14:29:35 +0400

    Initial commit from WeatherApp

--- app/src/main/java/com/kosenin/weatherapp2/Helpers/Rain.java
+++ app/src/main/java/com/kosenin/weatherapp2/Helpers/Rain.java
@@ -1,1 +1,8 @@
-package com.kosenin.weatherapp2.Helpers;/** * Created by Konstantin2 on 25.06.2017. */public class Rain {}
+package com.kosenin.weatherapp2.Helpers;
+
+/**
+ * Created by Konstantin2 on 25.06.2017.
+ */
+
+public class Rain {
+}
